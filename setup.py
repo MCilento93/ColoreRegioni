9e2eb7e48767753a5f9d1167bdda60f04a355cda
Mario Cilento <mario.cilento.93@gmail.com>
2020-12-07 23:32:35 +0100

    included gitignore

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     url="https://github.com/MCilento93/ColoreRegioni",
     packages=setuptools.find_packages(),
     install_requires=['beautifulsoup4','urllib'],
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3.8.5",
         "License :: OSI Approved :: MIT License",
