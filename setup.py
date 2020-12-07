34dac3b00d279bdc0a1cd7ef3f1a3f1989c23823
Mario Cilento <mario.cilento.93@gmail.com>
2020-12-08 00:05:34 +0100

    tar e whl

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     install_requires=['beautifulsoup4','urllib'],
     license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3.8.5",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
