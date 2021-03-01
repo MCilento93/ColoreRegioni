93ad30b9eb6b7837de3083bf75d95a30fece2d2e
Mario Cilento <mario.cilento.93@gmail.com>
2021-03-01 18:44:09 +0100

    included mangling for 'white' regions fixes

--- ColoreRegioni/__init__.py
+++ ColoreRegioni/__init__.py
@@ -22,7 +22,7 @@ class ColoreRegioni():
                 return 'giallo','🟡'
             elif 'verde' in onclick:
                 return 'verde','🟢'
-            elif '' in onclick:
+             '' in onclick:
                 return 'bianca','⚪️'
         else:
             return None,None
